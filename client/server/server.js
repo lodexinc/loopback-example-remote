eb38dac0b24d8acc4bcaeb13dfd999e615560187
crandmck <rand@strongloop.com>
2015-01-13 14:56:16 -0800

    Initial commit

---
+++ client/server/server.js
@@ -0,0 +1,29 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname);
+
+// call a method on the server
+app.models.Person.create({
+  name: 'Fred'
+}, function(err, joe) {
+  console.log("Created Person...");
+  console.log(err || joe);
+});
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
